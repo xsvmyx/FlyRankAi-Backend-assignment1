8bc8a9394e52b2ef96fde7eebd83a2f803309ea8
xsvmyx <mahisamy04@gmail.com>
2026-08-12 12:17:02 +0100

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException , Response
 from tasks import tasks
 
-
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -28,13 +28,40 @@ def get_task(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
             return task
-    return HTTPException(status_code=404, detail="Task not found")
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
-
-@app.post("/tasks")
+ 
+@app.post("/tasks",status_code=201)
 def create_task(task: dict):
     task["id"] = len(tasks) + 1
     task["done"] = False
     tasks.append(task)
     return { "message": "Task created successfully" }
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: dict):
+
+    if set(task.keys()) - {"title", "done"}:
+        raise HTTPException(status_code=400, detail="Invalid body")
+    for tsk in tasks:
+        if tsk["id"] == task_id:
+            if "title" in task:
+                tsk["title"] = task["title"]
+            if "done" in task:
+                tsk["done"] = task["done"]
+            return tsk
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+
+@app.delete("/tasks/{task_id}",status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return Response(status_code=204)
+    return HTTPException(status_code=404, detail="Task not found")
+    
